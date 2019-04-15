aab9b33a345a6a2ba749c045e84fd08cd2f7e22c
youn2029 <cocoa8090@gmail.com>
2019-04-15 14:50:38 +0900

    초기화 메소드 구현

---
+++ Custom-CSStepper/CSStepper.swift
@@ -0,0 +1,23 @@
+//
+//  CSStepper.swift
+//  Custom-CSStepper
+//
+//  Created by 윤성호 on 15/04/2019.
+//  Copyright © 2019 윤성호. All rights reserved.
+//
+
+import UIKit
+
+public class CSStepper: UIView {
+
+    // 스토리보드에서 호출할 초기화 메소드
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    // 프로그래밍 방식으로 호출하는 초기화 메소드
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+}
